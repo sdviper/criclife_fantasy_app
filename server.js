ef5bf5d6d1a46e300909750e97c5349f6bfa756c
Soumya <soumya.somu97@gmail.com>
2021-04-28 06:09:14 +0530

    heroku changes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// server.js
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/cric-fl'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/cric-fl/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
